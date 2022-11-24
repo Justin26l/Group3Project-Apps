d4c408ad988ad5f18331a44ea50400ad1b725a83
justin26l <laichimeng2001@gmail.com>
2022-11-25 00:00:52 +0800

    minor fix
    syntax error fix

--- app/tab4/tab4.module.ts
+++ app/tab4/tab4.module.ts
@@ -1,12 +1,11 @@
+import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
+import { Tab4Page } from './tab4.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab4PageRoutingModule } from './tab4-routing.module';
-
-import { Tab4Page } from './tab4.page';
 
 @NgModule({
   imports: [
