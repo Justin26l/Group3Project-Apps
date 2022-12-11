c37e68ca2e5109c435f6edf6309cb36820748cd1
justin26l <laichimeng2001@gmail.com>
2022-12-11 14:45:26 +0800

    fixed data in

--- app/tab4/tab4.page.ts
+++ app/tab4/tab4.page.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
+  public about = {
+    "status": "ok",
+    "error": "",
+    "result": [
+      {
+        "logo": "http://localhost/Group3Project/asset/img/logo.svg",
+        "company_name": "McDonald",
+        "description": "From the taste of our food and its halal ingredients,<br>right down to our restaurant cleanliness,<br>dedicated crew and grassroots community programmes,<br>we’re dedicated to bring you and your family an experience you can feel good about.",
+        "customer_service_no": "012 3456 7890",
+        "bussiness_name": "John Cena",
+        "bussiness_no": "012 3456 7890"
+      }
+    ]
+  };
+
   constructor() { }
 
   ngOnInit() {
